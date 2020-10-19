27712a5e162641758c0ea54570860bcc0977bebf
Rodrigo Fernandes Muniz <rodrigo.muniz.dev@gmail.com>
2020-10-19 19:48:41 -0300

    Finalizada a tarefa

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -13,7 +13,7 @@ namespace CursoCSharp
             Console.WriteLine("Entre com os dados do trabalhador: ");
             Console.Write("Nome: ");
             var nome = Console.ReadLine();
-            Console.Write("Nível (Junior = 0, Pleno = 1, Senior = 2: ");
+            Console.Write("Nível (Junior = 0, Pleno = 1, Senior = 2): ");
             var level = Enum.Parse<WorkerLevel>(Console.ReadLine());
             Console.Write("Salário base: ");
             var salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
@@ -27,7 +27,7 @@ namespace CursoCSharp
             for(int i =0; i<n; i++)
             {
                 Console.WriteLine($"Entre com o contrato n° {i}: ");
-                Console.Write("Data: (dd/mm/yyyy)");
+                Console.Write("Data: (dd/mm/yyyy): ");
                 var date = DateTime.Parse(Console.ReadLine());
                 Console.Write("Valor por hora: ");
                 var valorPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
@@ -37,8 +37,14 @@ namespace CursoCSharp
                 var contract = new HourContract(date, valorPorHora, horas);
 
                 worker.AddContract(contract);
-
             }
+            Console.WriteLine();
+            Console.Write("Entre com o mês e ano para calcular o valor a recebido: ");
+            string mesAno = Console.ReadLine();
+            var mes = int.Parse(mesAno.Substring(0, 2));
+            var ano = int.Parse(mesAno.Substring(3));
+
+            Console.WriteLine($"Nome: {nome}\nDepartamento: {worker.Department.Name}\nTotal recebido para {mesAno}: {worker.Income(ano,mes).ToString("F2", CultureInfo.InvariantCulture)}");
         }
     }
 }
